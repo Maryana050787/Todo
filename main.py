e04ed1838efdf633b4dee7550c08b03e48a83950
Rostislav2007 <kukolevrostislav@gmail.com>
2024-10-08 21:48:05 +0300

    add get_set

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ class Todo:
     def __init__(self):
         self.issue = {}
         self.count = 0
+        self.count_no_complete = 0
+        self.count_transfer = 0
         
-    def add_issue(self, issue, clock, complete):
-        self.issue[issue] = [clock, complete]
+    def add_issue(self, issue, clock, complete, transfer):
+        self.issue[issue] = [clock, complete, transfer]
         self.count += 1
         
     def delete_issue(self, issue):
@@ -15,21 +17,45 @@ class Todo:
     def get_count(self):
         return self.count
     
+    def get_count_no_complete(self):
+        return self.count_no_complete
+    
+    def get_count_transfer(self):
+        return self.count_transfer
+    
+    def set_count_no_complete(self, count_no_complete):
+        self.count_no_complete = count_no_complete
+        
+    def set_count_transfer(self, count_transfer):
+        self.count_transfer = count_transfer
+    
     def change_issue(self, issue):
         temp = self.issue.get(issue)
         temp[1] = True
         self.issue.update({issue:temp})
         
+    def change_transfer(self, issue):
+        temp = self.issue.get(issue)
+        temp[2] = True
+        self.issue.update({issue:temp})
+        
     def show(self):
         nums = 1
         for key, value in self.issue.items():
-            print(f'{nums}.{key} time:{value[0]} {'Выполнено'if value[1] else 'Не выполнено'}')
+            print(f'{nums}.{key} time:{value[0]} {'Выполнено'if value[1] else 'Не выполнено'} {'Перенесено'if value[2] else 'Не перенесено'}')
             nums +=1
             
 todo = Todo()
-todo.add_issue('Завтрак', '8:30', True)
-todo.add_issue('Уборка', '10:00', False)
-todo.add_issue('Корм. кот.', '12:30', False)
+todo.add_issue('Завтрак', '8:30', True, False)
+todo.add_issue('Уборка', '10:00', False, False )
+todo.add_issue('Корм. кот.', '12:30', False, False)
 todo.show()
 todo.change_issue('Уборка')
 todo.show()
+
+
+todo.set_count_transfer(10)
+todo.set_count_no_complete(14)
+
+print(todo.get_count_transfer())
+print(todo.get_count_no_complete())
